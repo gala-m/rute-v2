45b137e6143dd9a6aefed123853aee2f370b59ed
Gala <76803540+gala-m@users.noreply.github.com>
2022-03-20 19:35:55 +0200

    Delete dist directory

--- dist/index.2cd50cb1.js
+++
@@ -1,5 +0,0 @@
-var config = {
-    MAPBOX_TOKEN: 'pk.eyJ1Ijoid2lubmlhdHRoZXBhcmsiLCJhIjoiY2tocWxwYjB3MGFkeTJxcGJ6cDZzd285NCJ9.gZ7tGDVxt_ArW9WptTgK8A'
-};
-
-//# sourceMappingURL=index.2cd50cb1.js.map
